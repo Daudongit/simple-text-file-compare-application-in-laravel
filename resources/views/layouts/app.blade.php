e04e334be64c8450a08320b90a3d6b1fb59caf40
Daud <daudonmail@gmail.com>
2019-05-31 18:06:11 +0100

    Change app title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Copy Compare App') }}</title>
+    <title>TextCompareApp</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -30,7 +30,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        Copy Compare App
+                         Compare files App
                     </a>
                 </div>
 
